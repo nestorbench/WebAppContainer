04e26aec783bc1237d80ed7119b7001ee848ddae
Nestor <nestor.bench@gmail.com>
2021-02-15 01:13:10 -0500

    clean up

--- status/CompareUpTime.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-ReadyTime=10
-AppUptime=$((`cut -d. -f1 /proc/uptime`-`cat /var/www/html/status/StartTime`))
-[[ $AppUptime -lt $ReadyTime ]]&&IsReady='{"ready": false}'
-[[ $AppUptime -ge $ReadyTime ]]&&IsReady='{"ready": true}'
-[[ $IsReady == "" ]]&&echo " ERROR: Uptime check failed. Please check distro"
-echo $IsReady 
-
