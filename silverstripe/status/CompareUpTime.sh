8f0e7484c7bae235ecfebae9a29cd9870fe388a9
Nestor <nestor.bench@gmail.com>
2021-02-15 01:53:31 -0500

    Change Uptime calculation method

--- silverstripe/status/CompareUpTime.sh
+++ silverstripe/status/CompareUpTime.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 ReadyTime=10
-AppUptime=$((`cut -d. -f1 /proc/uptime`-`cat /var/www/html/status/StartTime`))
+#AppUptime=$((`cut -d. -f1 /proc/uptime`-`cat /var/www/html/status/StartTime`))
+
+PID=`ps -ef | grep apache2|grep www-data|awk {'print $2'}|head -1`
+AppUptime=`ps -o etimes= -p "$PID"`
 [[ $AppUptime -lt $ReadyTime ]]&&IsReady='{"ready": false}'
 [[ $AppUptime -ge $ReadyTime ]]&&IsReady='{"ready": true}'
 [[ $IsReady == "" ]]&&echo " ERROR: Uptime check failed. Please check distro"
